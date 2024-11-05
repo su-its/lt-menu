0d5342df253a22f3dc00d3952ebf25c5fac05d82
KinjiKawaguchi <kawakin0310@icloud.com>
2024-11-05 18:54:12 +0900

    chore: install biome and format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,34 +3,34 @@ import localFont from "next/font/local";
 import "./globals.css";
 
 const geistSans = localFont({
-	src: "./fonts/GeistVF.woff",
-	variable: "--font-geist-sans",
-	weight: "100 900",
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
 });
 const geistMono = localFont({
-	src: "./fonts/GeistMonoVF.woff",
-	variable: "--font-geist-mono",
-	weight: "100 900",
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
 });
 
 export const metadata: Metadata = {
-	title: "テクノフェスタ2024 静大LT Powered by ITS",
-	description:
-		"静岡大学テクノフェスタ2024でITソルーション室主催のLT大会の紹介ページ",
+  title: "テクノフェスタ2024 静大LT Powered by ITS",
+  description:
+    "静岡大学テクノフェスタ2024でITソルーション室主催のLT大会の紹介ページ",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="ja">
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
-				{children}
-			</body>
-		</html>
-	);
+  return (
+    <html lang="ja">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
 }
