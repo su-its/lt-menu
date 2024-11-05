53a366b416d689727d4e35626bb38055ad6bbfe4
かわきん(河口欣仁) <93261102+KinjiKawaguchi@users.noreply.github.com>
2024-11-05 16:53:51 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -86,7 +86,7 @@ export default function Component() {
 					>
 						<div className="flex items-center">
 							<Calendar className="w-5 h-5 mr-2" />
-							<span className="font-semibold">Date: 11/9</span>
+							<span className="font-semibold">Date: 11/9(土曜)</span>
 						</div>
 						<div className="flex items-center">
 							<Clock
