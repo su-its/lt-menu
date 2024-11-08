edec6147f93cc18e2f9a8d67f77408ef49bd0117
KinjiKawaguchi <kawakin0310@icloud.com>
2024-11-09 05:02:41 +0900

    fix: スライドのURLを直接使用するように修正

--- src/app/[year]/lt/[id]/page.tsx
+++ src/app/[year]/lt/[id]/page.tsx
@@ -81,7 +81,7 @@ export default function TalkDetail({
               <p>スライド公開予定</p>
             ) : (
               <iframe
-                src={`https://speakerdeck.com/player/${talk.slideUrl}`}
+                src={`${talk.slideUrl}`}
                 allowFullScreen
                 title={`${talk.exhibit.name} presentation`}
                 className="w-full h-full rounded-lg min-h-[500px]"
