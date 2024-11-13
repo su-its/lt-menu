ac42376cf53fb5217bbb615ba9ac5a80f731b434
KinjiKawaguchi <kawakin0310@icloud.com>
2024-11-13 18:34:39 +0900

    SSG

--- src/app/[year]/exhibit/page.tsx
+++ src/app/[year]/exhibit/page.tsx
@@ -1,56 +1,43 @@
-"use client";
-
-import { motion } from "framer-motion";
 import { Calendar } from "lucide-react";
 import { getExhibits, type ExhibitWithAll } from "../../actions/exhibit";
 import type { Event } from "@shizuoka-its/core";
 import { exhibit_data_table } from "@/constants";
-import { use, useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { formatDateToYYYYMMDD } from "@/libs/dateUtil";
-import { Spinner } from "@/Components/Spinner";
 import { getEvent } from "@/app/actions/lt";
+import { notFound } from "next/navigation";
 
-export default function ExhibitList({
-  params,
-}: {
-  params: Promise<{ year: string }>;
-}) {
-  const router = useRouter();
-  const { year } = use(params);
-  const [exhibits, setExhibits] = useState<ExhibitWithAll[]>([]);
-  const [event, setEvent] = useState<Event | null>(null);
+// ページの再生成間隔を設定
+export const revalidate = 3600; // 1時間ごとに再生成
 
-  useEffect(() => {
-    const fetchExhibits = async () => {
-      const eventID = exhibit_data_table.find(
-        (exhibit) => exhibit.year === year,
-      )?.id as string;
-      const event = await getEvent(eventID);
-      const exhibits = await getExhibits(event);
-      if (exhibits && event) {
-        setExhibits(exhibits);
-        setEvent(event);
-      } else {
-        router.push("/404");
-      }
-    };
-    fetchExhibits();
-  }, [year, router]);
+// 動的なパラメータの生成
+export async function generateStaticParams() {
+  return exhibit_data_table.map((exhibit) => ({
+    year: exhibit.year,
+  }));
+}
 
+type tParams = Promise<{ year: string }>;
+
+async function ExhibitList(props: { params: tParams }) {
+  const { year } = await props.params;
+  const eventID = exhibit_data_table.find((exhibit) => exhibit.year === year)
+    ?.id as string;
+
+  const event = await getEvent(eventID);
   if (!event) {
-    return <Spinner />;
+    notFound();
   }
+
+  const exhibits = await getExhibits(event);
+  if (!exhibits) {
+    notFound();
+  }
+
   return (
     <div className="bg-white text-black min-h-screen p-8 font-sans overflow-hidden">
       <div className="max-w-6xl mx-auto">
-        <motion.header
-          className="text-center mb-12 mt-16"
-          initial={{ opacity: 0, y: -50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5 }}
-        >
+        <header className="text-center mb-12 mt-16">
           <h1 className="text-3xl lg:text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-purple-600 mb-4">
             {event?.name} 展示品一覧
           </h1>
@@ -62,17 +49,12 @@ export default function ExhibitList({
               </span>
             </div>
           </div>
-        </motion.header>
+        </header>
         <main>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-            {exhibits.map((exhibit, index) => (
+            {exhibits.map((exhibit) => (
               <Link href={`/${year}/exhibit/${exhibit.id}`} key={exhibit.id}>
-                <motion.div
-                  className="h-full bg-white rounded-lg p-6 shadow-lg hover:shadow-2xl transition-shadow duration-300 border-2 border-gray-300 cursor-pointer"
-                  initial={{ opacity: 0, scale: 0.9 }}
-                  animate={{ opacity: 1, scale: 1 }}
-                  transition={{ duration: 0.3, delay: index * 0.1 }}
-                >
+                <div className="h-full bg-white rounded-lg p-6 shadow-lg hover:shadow-2xl transition-shadow duration-300 border-2 border-gray-300 cursor-pointer">
                   <h2 className="text-2xl font-bold mb-2 text-purple-700">
                     {exhibit.name}
                   </h2>
@@ -96,22 +78,19 @@ export default function ExhibitList({
                       </a>
                     </div>
                   )}
-                </motion.div>
+                </div>
               </Link>
             ))}
           </div>
         </main>
-        <motion.footer
-          className="text-center mt-12"
-          initial={{ opacity: 0, y: 50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5, delay: 0.5 }}
-        >
+        <footer className="text-center mt-12">
           <p className="text-xl text-gray-800 mb-4 font-bold">
             素晴らしい展示をお楽しみください！
           </p>
-        </motion.footer>
+        </footer>
       </div>
     </div>
   );
 }
+
+export default ExhibitList;
